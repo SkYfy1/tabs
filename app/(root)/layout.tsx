250d92ba15cde80fe5bffad32f1d6ccf14ad0f7e
SkYfy1 <festik.tawer@gmail.com>
2025-04-16 21:55:36 +0300

    refactor(app) - layout + main page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="pt-20 ml-12">
+    <div className="pt-20">
       <TabsContainer />
       <main className="bg-gray-bg h-[1000px] p-5">{children}</main>
     </div>
