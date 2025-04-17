2cc6c7be0227ebeb147d1f8a0c7d5f8472c9f4f9
SkYfy1 <festik.tawer@gmail.com>
2025-04-18 00:07:00 +0300

    refactor(app/layout) - layout fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,9 +3,9 @@ import React, { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="pt-20">
+    <div className="pt-1 min-h-screen h-screen flex flex-col">
       <TabsContainer />
-      <main className="bg-gray-bg h-[1000px] p-5">{children}</main>
+      <main className="bg-gray-bg p-5 flex-1">{children}</main>
     </div>
   );
 };
